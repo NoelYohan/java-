f4c7bf0387b7c120208fc2788a19d1468aab2488
Noel Yohannan <122074908+NoelYohan@users.noreply.github.com>
2024-11-16 11:38:17 +0530

    Update and rename sample.java to FindMin and MaxCP1.java

--- FindMin and MaxCP1.java
+++ FindMin and MaxCP1.java
@@ -1,4 +1,12 @@
-public class sample {
+/*Input: arr[] = {3, 5, 4, 1, 9}
+Output: Minimum element is: 1
+              Maximum element is: 9
+
+
+Input: arr[] = {22, 14, 8, 17, 35, 3}
+Output:  Minimum element is: 3
+              Maximum element is: 35
+*/public class sample {
     public static void main(String[] args) {
         int a[]={22, 14, 8, 17, 35, 3};
         int min=a[0];
